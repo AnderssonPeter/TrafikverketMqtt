e3ae9679038a249ccb1dfcf064c834a16ad443b3
Peter Andersson <petoj87@gmail.com>
2018-08-20 20:36:29 +0200

    simplified mqtt sending to fix deadlock.

--- TrafikverketMQTT/MqttSender.cs
+++ TrafikverketMQTT/MqttSender.cs
@@ -31,34 +31,32 @@ namespace TrafikverketMQTT
 
         private async Task<MqttClient> GetClient()
         {
-            using (await locker.LockAsync())
+            if (client != null && !client.IsConnected)
             {
-                if (client != null && !client.IsConnected)
-                {
-                    client = null;
-                }
-                if (client == null)
-                {
-                    logger.LogTrace("Connecting to MQTT server");
-                    client = await Task.Run(() =>
-                    {
-                        var client = settings.Port.HasValue ? new MqttClient(settings.HostAddress, settings.Port.Value, false, MqttSslProtocols.None, null, null) : new MqttClient(settings.HostAddress);
-                        logger.LogDebug("Connected to MQTT server, authenticating");
-                        if (settings.UserName != null) {
-                            client.Connect(settings.ClientId, settings.UserName, settings.Password);
-                        }
-                        else
-                        {
-                            client.Connect(settings.ClientId);
-                        }
-                        return client;
-                    });
-
-                    logger.LogDebug("Authenticated with MQTT server");
-                }
-                return client;
+                client = null;
             }
+            if (client == null)
+            {
+                logger.LogTrace("Connecting to MQTT server");
+                client = await Task.Run(() =>
+                {
+                    var client = settings.Port.HasValue ? new MqttClient(settings.HostAddress, settings.Port.Value, false, MqttSslProtocols.None, null, null) : new MqttClient(settings.HostAddress);
+                    logger.LogDebug("Connected to MQTT server, authenticating");
+                    if (settings.UserName != null) {
+                        client.Connect(settings.ClientId, settings.UserName, settings.Password);
+                    }
+                    else
+                    {
+                        client.Connect(settings.ClientId);
+                    }
+                    return client;
+                });
+
+                logger.LogDebug("Authenticated with MQTT server");
+            }
+            return client;
         }
+
         JsonSerializerSettings jsonSettings = new JsonSerializerSettings()
         {
             ContractResolver = new DefaultContractResolver
@@ -72,12 +70,13 @@ namespace TrafikverketMQTT
         {
             logger.LogTrace("Sending payload to MQTT server");
             var payload = encoding.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(data, jsonSettings));
-            var client = await GetClient();
-            await Task.Run(() =>
+
+            using (await locker.LockAsync())
             {
+                var client = await GetClient();
                 client.Publish(settings.BaseTopic + "/" + topic, payload, (byte)settings.QualityOfService.GetValueOrDefault(MqttQualityOfService.AtLeastOnce), settings.RetainLastMessageOnServer.GetValueOrDefault(false));
-            });
-            logger.LogDebug("MQTT message sent");
+                logger.LogDebug("MQTT message sent");
+            }
         }
 
         #region IDisposable Support
